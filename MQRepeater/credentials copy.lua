3c0c5a186a9a8240221cbd61853e8c6dbe573d94
Alex Savva <savvyaxl@yahoo.com>
2024-07-14 18:26:19 -0300

    creds

---
+++ MQRepeater/credentials copy.lua
@@ -0,0 +1,18 @@
+-- Credentials
+SSID     = "SSID"
+PASSWORD = "password"
+MQTTHOST = 'mq host'
+MQTTPORT = 1883
+MQTTUSER = 'admin'
+MQTTPASS = 'admin'
+
+credentials = {
+    ["829D_Fibra"] = {
+        SSID     = "SSID",
+        PASSWORD = "password",
+        MQTTHOST = 'mq host',
+        MQTTPORT = 1883,
+        MQTTUSER = 'admin',
+        MQTTPASS = 'admin'
+    },
+}
