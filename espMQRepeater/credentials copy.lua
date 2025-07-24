6240129be116e48907fd049a590680d747247638
Alex Savva <savvyaxl@yahoo.com>
2025-07-23 23:13:58 -0300

    new format

--- espMQRepeater/credentials copy.lua
+++ espMQRepeater/credentials copy.lua
@@ -1,16 +1,41 @@
 -- Credentials
-SSID     = "SSID"
-PASSWORD = "password"
-MQTTHOST = 'mq host'
-MQTTPORT = 1883
-MQTTUSER = 'admin'
-MQTTPASS = 'admin'
-
 credentials = {
     ["829D_Fibra"] = {
-        SSID     = "SSID",
-        PASSWORD = "password",
-        MQTTHOST = 'mq host',
+        SSID     = "829D_Fibra",
+        PASSWORD = "xx",
+        MQTTHOST = 'xx',
+        MQTTPORT = 1883,
+        MQTTUSER = 'admin',
+        MQTTPASS = 'admin'
+    },
+    ["GREEN"] = {
+        SSID     = "GREEN",
+        PASSWORD = "xx",
+        MQTTHOST = 'xx',
+        MQTTPORT = 1883,
+        MQTTUSER = 'admin',
+        MQTTPASS = 'admin'
+    },
+    ["GREEN5"] = {
+        SSID     = "GREEN5",
+        PASSWORD = "xx",
+        MQTTHOST = 'xx',
+        MQTTPORT = 1883,
+        MQTTUSER = 'admin',
+        MQTTPASS = 'admin'
+    },
+    ["TIM_ULTRAFIBRA_453F_2G"] = {
+        SSID     = "TIM_ULTRAFIBRA_2G",
+        PASSWORD = "xx",
+        MQTTHOST = 'xx',
+        MQTTPORT = 1883,
+        MQTTUSER = 'admin',
+        MQTTPASS = 'admin'
+    },
+    ["TIM_ULTRAFIBRA_453F_5G"] = {
+        SSID     = "TIM_ULTRAFIBRA_5G",
+        PASSWORD = "xx",
+        MQTTHOST = 'xx',
         MQTTPORT = 1883,
         MQTTUSER = 'admin',
         MQTTPASS = 'admin'
