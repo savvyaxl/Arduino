e9ef8bd88e596ec66e516ade28bab0fe13c38d00
Alex Savva <savvyaxl@yahoo.com>
2026-05-05 23:15:53 -0300

    RC 1

--- ESP32/boardS2Mini/testAlarms3.py
+++ ESP32/boardS2Mini/testAlarms3.py
@@ -1,7 +1,6 @@
 import uasyncio as asyncio # type: ignore
 from alex.SmartHomeManager import SmartHomeManager
 
-
 # --- EXECUTION ---
 # Ensure you connect to WiFi first in boot.py or main.py
 manager = SmartHomeManager(utc_offset=-3)
